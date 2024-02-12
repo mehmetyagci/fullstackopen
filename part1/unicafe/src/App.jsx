ae0c5947ee7576ea9668bb4916c61d01a002f2ba
mehmetyagci <mehmetyagci53@gmail.com>
2024-02-12 22:35:54 +0300

    exercises 1.9: unicafe step4 completed.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Statistics = (props) => {
+  console.log('Statistics')
+
+  if (props.allFeedbacks.length === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  const goodFeedbacksCount = props.allFeedbacks.filter(feedback => feedback === 'G').length;
+  console.log('goodFeedbacksCount', goodFeedbacksCount)
+  const neutralFeedbacks = props.allFeedbacks.filter(feedback => feedback === 'N').length;
+  console.log('neutralFeedbacks', neutralFeedbacks)
+  const badFeedbacks = props.allFeedbacks.filter(feedback => feedback === 'B').length;
+  console.log('badFeedbacks', badFeedbacks)
+
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>statistics</h1>
+      <div> good {goodFeedbacksCount}</div> 
+      <div> neutral {neutralFeedbacks}</div> 
+      <div> bad {badFeedbacks}</div> 
+      <div> all {props.allFeedbacks.length}</div> 
+      <div> average {(goodFeedbacksCount + (-1*badFeedbacks)) / props.allFeedbacks.length}</div> 
+      <div> positive {(goodFeedbacksCount / props.allFeedbacks.length)*100} %</div> 
+    </div>
+  )
+}
+
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const [allFeedbacks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleGoodClick = () => {
+    setAll(allFeedbacks.concat('G'))
+    console.log('good before', good)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    console.log('good after', good)
+    setTotal(updatedGood + neutral + bad) 
+  }
+
+  const handleNeutralClick = () => {
+    setAll(allFeedbacks.concat('N'))
+    console.log('neutral before', neutral)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    console.log('neutral after', neutral)
+    setTotal(good + updatedNeutral + bad) 
+  }
+
+
+  const handleBadClick = () => {
+    setAll(allFeedbacks.concat('B'))
+    console.log('bad before', bad)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    console.log('bad after', bad)
+    setTotal(good + neutral + updatedBad) 
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <Statistics allFeedbacks={allFeedbacks} />
+  </div>
   )
 }
 

9ed2ef0cd326f6d4dc6c24c80bb47c3b2cf3b9c8
mehmetyagci <mehmetyagci53@gmail.com>
2024-02-14 15:40:41 +0300

    2.7: The Phonebook Step 2 completed.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Person from './components/Person'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingPerson = persons.some(person => person.name === newName);
+    console.log('existingPerson', existingPerson)
+    if (existingPerson) {
+      alert(`${newName} is already added to phonebook`)
+      return;
+    }
+    console.log(`Adding ${newName} since it doesn't exist in the phonebook.`);
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    console.log('button clicked', event.target)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <input value={newName} onChange={handleNameChange} />
+        <button type="submit">add</button>
+      </form> 
+      
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => 
+          <Person key={index} person={person} />
+        )}
+      </ul>
+    </div>
   )
 }
 

d6ceb1f9503efa5b19412b2fbf5b4a9c9ded7bf5
mehmetyagci <mehmetyagci53@gmail.com>
2024-02-20 12:45:24 +0300

    3.2: Phonebook backend step 2

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -32,6 +32,11 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/info', (request, response) => {
+    const info = `Phonebook has info for ${persons.length} people <br/> ${new Date()}`
+    response.send(info)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)

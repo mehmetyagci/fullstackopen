f9e2b366bfc2d38cf74057178ece77136065c33a
mehmetyagci <mehmetyagci53@gmail.com>
2024-03-07 12:56:55 +0300

    4.19: Blog List Expansion, step 7 completed.

--- part4/bloglistbackend/tests/test_helper.js
+++ part4/bloglistbackend/tests/test_helper.js
@@ -1,5 +1,5 @@
 const Blog = require('../models/blog')
-
+const User = require('../models/user')
 const initialBlogs = [
   {
     _id: '5a422a851b54a676234d17f7',
@@ -64,6 +64,11 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
 module.exports = {
-  initialBlogs, nonExistingId, blogsInDb
+  initialBlogs, nonExistingId, blogsInDb, usersInDb
 }
